c705252d782dbbf732910107dd86f5b17efef846
Kiet Ho <hoakiet98@gmail.com>
2025-10-26 05:38:27 +0800

    blog

--- apps/website/mdx-components.tsx
+++ apps/website/mdx-components.tsx
@@ -1,5 +1,10 @@
+import { useMDXComponents as getBlogMDXComponents } from "nextra-theme-blog";
 import { useMDXComponents as getDocsMDXComponents } from "nextra-theme-docs";
 
 export function useMDXComponents(components = {}) {
-	return getDocsMDXComponents(components);
+	// Merge both theme components to support both /docs and /blog routes
+	return {
+		...getDocsMDXComponents(components),
+		...getBlogMDXComponents(components),
+	};
 }
